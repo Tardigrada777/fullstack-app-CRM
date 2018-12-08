30df0e35404ddfaaf59e53b36aba51c829658720
tardigrada777 <ntwrite@gmail.com>
2018-12-09 01:08:35 +0300

    Подключил еще два пакета (cors и morgan)

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
+// Базовые пакеты
 const express = require('express');
+
+// Роуты
 const authRoutes = require('./routes/auth');
 const analyticsRoutes = require('./routes/analytics');
 const categoryRoutes = require('./routes/category');
 const orderRoutes = require('./routes/order');
 const positionRoutes = require('./routes/position');
 
+// Вспомогательные плагины
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Для обработки запросов с других доменов
+const morgan = require('morgan'); // Для логирования работы сервера
+
+
 
 const app = express();
 
+
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
