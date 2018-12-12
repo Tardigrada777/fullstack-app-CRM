aa8d6fe72834c4cdafd69f59952519110e164002
tardigrada777 <ntwrite@gmail.com>
2018-12-12 21:56:10 +0300

    Обработал получение и удаление категорий в контроллере Category

--- controllers/category.js
+++ controllers/category.js
@@ -1,19 +1,50 @@
-module.exports.getAll = (req, res) => {
-    res.json({message: 'Category'})
+const Category = require('../models/Category')
+const errorHandler = require('../utils/errorHandler')
+const Position = require('../models/Position')
+
+module.exports.getAll = async (req, res) => {
+   try {
+       const categories = await new Category.find({user: req.user.id})
+       res.status(200).json(categories)
+   } catch (error) {
+       errorHandler(res, error)
+   }
 }
 
-module.exports.getById = (req, res) => {
-    
+module.exports.getById = async (req, res) => {
+    try {
+        const category = await new Category.findById(req.params.id)
+        res.status(200).json(category)
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
-module.exports.remove = (req, res) => {
-    
+module.exports.remove = async (req, res) => {
+    try {
+       await Category.remove({_id: req.params.id})
+       await Position.remove({category: req.params.id})
+
+       res.status(200).json({
+           message: 'Категория удалена'
+       })
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
 module.exports.create = (req, res) => {
-    
+    try {
+       
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
 
 module.exports.update = (req, res) => {
-    
+    try {
+       
+    } catch (error) {
+        errorHandler(res, error)
+    }
 }
