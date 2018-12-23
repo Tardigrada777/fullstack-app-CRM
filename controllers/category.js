e16538f013e6865cf2346fb053a3ac3a2a5c4298
tardigrada777 <ntwrite@gmail.com>
2018-12-23 17:04:05 +0300

    Дописал логику добавления и обновления файлов и категорий

--- controllers/category.js
+++ controllers/category.js
@@ -33,17 +33,35 @@ module.exports.remove = async (req, res) => {
     }
 }
 
-module.exports.create = (req, res) => {
+module.exports.create = async (req, res) => {
+    const category = new Category({
+        name: req.body.name,
+        user: req.user.id,
+        imageSrc: req.file ? req.file.path : ''
+    })
     try {
-       
+        console.log(req)
+       await category.save()
+       res.status(201).json(category)
     } catch (error) {
         errorHandler(res, error)
     }
 }
 
-module.exports.update = (req, res) => {
+module.exports.update = async (req, res) => {
+    const updated = {
+        name: req.body.name
+    }
+    if (req.file){
+        updated.imageSrc = req.file.path
+    }
     try {
-       
+       const category = await Category.findOneAndUpdate(
+           {_id: req.params.id},
+           {$set: updated},
+           {new: true}
+       )
+       res.status(200).json(category)
     } catch (error) {
         errorHandler(res, error)
     }
